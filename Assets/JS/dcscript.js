41b3aba44c663cfbcd2642b772bcc52241958783
Karan Singh Dhir <ksinghdhir1@gmail.com>
2017-09-13 13:55:55 +0530

    Display buttons renders new charts when clicked again

---
+++ Assets/JS/dcscript.js
@@ -0,0 +1,35 @@
+var result = [];
+var button = document.getElementById("displayData");
+var firstFrame = document.getElementById("one");
+var displayFrames = document.getElementById("frames");
+
+button.addEventListener("click", function(){
+
+    var divElem = document.getElementById("frames");
+    $("#frames").empty();
+
+    var select = document.getElementsByTagName('select')[0];
+    var options = select && select.options;
+    var opt;
+    console.log(options)
+
+  for (var i=0; i<options.length; i++) {
+    opt = options[i];
+
+    if (opt.selected && result.length < 4) {
+      result.push(opt.value);
+    }
+  }
+  console.log(result);
+
+
+  for(var i = 0; i < result.length; i++) {
+    var frame = document.createElement("iframe");
+    frame.src= "templates/"+result[i];
+    console.log(frame.src);
+    frame.width = "600"; frame.height = "600";
+    displayFrames.appendChild(frame);
+  }
+
+result=[];
+})
