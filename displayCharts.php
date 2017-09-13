f614031c0a2f4ad4b950aedfe259e3c807f6bb80
Karan Singh Dhir <ksinghdhir1@gmail.com>
2017-09-13 12:51:51 +0530

    added frames to display the chart(s)

--- displayCharts.php
+++ displayCharts.php
@@ -1,11 +1,14 @@
-
+<html>
+    <head>
+    <link rel="stylesheet" href="styles.css">
+    </head>
+    <body>
 <select id="displayCharts" multiple>
     <option selected="">Choose one</option>
 <?php
 
 $dir    = 'F:\UniServerZ\www\servers\Templates';
 $files = scandir($dir);
-//print_r($files);
 foreach($files as $name) { ?>
     <option value="<?php echo $name ?>"><?php echo $name ?></option>
 <?php
@@ -13,14 +16,18 @@ foreach($files as $name) { ?>
 
 ?>
 </select>
-<button id="displayData">Display</button>
+<button id="displayData">Display</button><br>
+<div id="frames"></div>
+
 <script>
-var array = [];
+var result = [];
 var button = document.getElementById("displayData");
+var firstFrame = document.getElementById("one");
+var displayFrames = document.getElementById("frames");
 
 button.addEventListener("click", function(){
     var select = document.getElementsByTagName('select')[0];
-    var result = [];
+    
     var options = select && select.options;
     var opt;
     console.log(options)
@@ -28,14 +35,32 @@ button.addEventListener("click", function(){
   for (var i=0; i<options.length; i++) {
     opt = options[i];
 
-    if (opt.selected && result.length <4) {
-      result.push(opt.value || opt.text);
+    if (opt.selected && result.length < 4) {
+      result.push(opt.value);
     }
   }
   console.log(result);
 
 
+  for(var i = 0; i < result.length; i++) {
+    var frame = document.createElement("iframe");
+    frame.src= "templates/"+result[i];
+    console.log(frame.src);
+    frame.width = "600"; frame.height = "600";
+    displayFrames.appendChild(frame);
+  }
+
+result=[];
 })
 
 
+/*var secondFrame = document.getELementById("two");
+var thirdFrame = document.getELementById("three");
+var fourthFrame = document.getELementById("four");
+*/
+
+
+
 </script>
+</body>
+</html>
