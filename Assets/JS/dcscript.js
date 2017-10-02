7bd81e4ac5f2771a3afdc3034dc4afe11ffe0f75
Karan Singh Dhir <ksinghdhir1@gmail.com>
2017-10-02 21:49:58 +0530

    default_file added and individual table appended

--- Assets/JS/dcscript.js
+++ Assets/JS/dcscript.js
@@ -1,12 +1,13 @@
-$(document).ready(function(){
-
 var result = [];
+
+$(document).ready(function(){
 var button = document.getElementById("displayData");
 var firstFrame = document.getElementById("one");
 var displayFrames = document.getElementById("frames");
+var currentFrame;
 
 button.addEventListener("click", function(){
-
+    result = [];
     var divElem = document.getElementById("frames");
     $("#frames").empty();
 
@@ -17,7 +18,7 @@ button.addEventListener("click", function(){
   for (var i=0; i<options.length; i++) {
     opt = options[i];
 
-    if (opt.selected && result.length < 4 && opt.value != "Choose one") {
+    if (opt.selected && result.length < 4 && opt.value != "Select File(s)") {
       result.push(opt.value);
     }
   }
@@ -27,21 +28,20 @@ button.addEventListener("click", function(){
   var loadIframe = function() {
     if (fileNameCounter == result.length){
       syncScroll();
-      syncCursor();
       $("div.buttons").show(); 
-      result=[];
       return;
     }
     
     var frame = document.createElement("iframe");
-    frame.src = "Templates/"+ result[fileNameCounter];
+    frame.src = "default_file.html";
     frame.width = (window.innerWidth/2.20);
     frame.height = (window.innerHeight/1.90);
     displayFrames.appendChild(frame);
     
     
     frame.onload = function() {
-      console.log(result[fileNameCounter]);
+      currentFrame = frame;
+      fetchTable(result[fileNameCounter]);
       fileNameCounter++;
       loadIframe();
     }
@@ -51,7 +51,7 @@ button.addEventListener("click", function(){
   loadIframe();
 
   function syncScroll() {
-    console.log("scrollbars will be synced!");
+    //console.log("scrollbars will be synced!");
     var iframeArray = $("iframe").map(function(){return this});
      // syncs the scrollbars of all the iframes
      iframeArray.each(function(e){
@@ -69,21 +69,69 @@ button.addEventListener("click", function(){
     })
   };
 
-  function syncCursor() {
+function fetchTable (filename) {
+  var filelocation = "./Templates/"+filename;
+  readTextFile(filelocation);
+};
 
-    var iframeEl = document.getElementsByTagName("iframe");
-    iframeEl[0].contentWindow.document.body.addEventListener("click", function(event){
-      console.log(event.clientX,event.clientY);
-    });
 
+function readTextFile(file)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                //alert(allText);
+                get_table_contents(allText);
+
+            }
+        }
+    }
+    rawFile.send(null);
+}
+
+  function get_table_contents (filetext) {
+    var table_contents = "";
+    var condition = false;
+
+    for (var i = 0; i < filetext.length ; i++) {
+      
+      
+      if (filetext.charAt(i) == "<" && filetext.charAt(i+1) == "/" && filetext.charAt(i+2) == "b" && filetext.charAt(i+3) == "o" ) {
+        appendTable(table_contents);
+        table_contents = "";
+        break;
+      }
+      
+
+
+      if(filetext.charAt(i) == "<" && filetext.charAt(i+1) == "t" && filetext.charAt(i+2) == "a" && filetext.charAt(i+3) == "b" ) {
+        condition = true;
+      }
+
+      if (condition) {
+        table_contents += filetext.charAt(i);
+      }
+    
+    
+    };
+  };
+
+  function appendTable (table_contents) {
+    var table_div = currentFrame.contentWindow.document.getElementById("table");
+    table_div.innerHTML = table_contents;
   }
 
 
+}) // button click event closes
 
 
 
 
-})
 
-  $('#frames > button').hide();
-});
+});// window.ready closes
