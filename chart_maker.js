a18d67838a15405d99c6e6fbe3a20e2543096813
Karan Singh Dhir <ksinghdhir1@gmail.com>
2017-10-12 17:09:26 +0530

    more types of charts added

--- chart_maker.js
+++ chart_maker.js
@@ -5,7 +5,7 @@
 		function setupCharts() {
 
             //var button = parent.document.getElementById(dataPHP[2]).trigger("click");
-		
+		var types_of_charts = ["AreaChart","ColumnChart","BubbleChart"];
 		var chart = null;
 
 		var data = google.visualization.arrayToDataTable(dataPHP[0]);
@@ -15,11 +15,17 @@
 
 		document.getElementById(dataPHP[2]).addEventListener("click", function() {
 		if (chart && chart.clearChart) chart.clearChart();
+		var i = 0;
+		if(this.id == "draw_CPU_USE") {
+			i=1;
+		} else if (this.id == "draw_TOPSUM") {
+			i=2;
+		}
 
-		chart = new google.visualization.AreaChart(document.getElementById("chart_master"));
+		chart = new google.visualization[types_of_charts[i]](document.getElementById("chart_master"));
 		chart.draw( data, options);
 		parent.window.iframe_charts[element_id] = chart;
-
+		
 		google.visualization.events.addListener(chart,'select',function() {
 				parent.window.iframe_charts.forEach(function(element) {
 					element.setSelection(chart.getSelection());
