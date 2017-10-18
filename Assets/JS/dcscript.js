f585b8f2846b5f6262d7b6198fe3d66fbb9c1f99
Karan Singh Dhir <ksinghdhir1@gmail.com>
2017-10-18 13:13:34 +0530

    comments added

--- Assets/JS/dcscript.js
+++ Assets/JS/dcscript.js
@@ -1,11 +1,11 @@
-var result = [];
+var result = []; // this global array stores the selected file names
 
 $(document).ready(function(){
 var button = document.getElementById("displayData");
 var firstFrame = document.getElementById("one");
 var displayFrames = document.getElementById("frames");
 var currentFrame;
-window.iframe_charts = [];
+window.iframe_charts = []; // this array stores chart data -> used for syncing the mouse click event
 
 button.addEventListener("click", function(){
     result = [];
@@ -25,16 +25,16 @@ button.addEventListener("click", function(){
   }
 
   var fileNameCounter = 0;
-
+// a recursive loadIframe function is used in order to make the iframes load synchronously
   var loadIframe = function() {
     if (fileNameCounter == result.length){
       syncScroll();
-      $("div.buttons").show(); 
+      $("div.buttons").show();  //  the buttons are only displayed when all the iframes are loaded
       return;
     }
     
     var frame = document.createElement("iframe");
-    frame.src = "default_file.html";
+    frame.src = "default_file.html"; // a single html file is used as a blueprint for all the iframes -> data of different iframes will appened into this file
     frame.id = parseInt((fileNameCounter));
     frame.width = (window.innerWidth/2.20);
     frame.height = (window.innerHeight/1.90);
@@ -43,7 +43,7 @@ button.addEventListener("click", function(){
     
     frame.onload = function() {
       currentFrame = frame;
-      fetchTable(result[fileNameCounter]);
+      fetchTable(result[fileNameCounter]); // this function fetches the table element along with its children and appends it into the default file
       fecthFileName(result,fileNameCounter);
       fileNameCounter++;
       loadIframe();
@@ -80,31 +80,27 @@ button.addEventListener("click", function(){
 
 function fetchTable (filename) {
   var filelocation = "./Templates/"+filename;
-  readTextFile(filelocation);
+  readTextFile(filelocation); 
 };
 
 
-function readTextFile(file)
-{
+function readTextFile(file) {
+  // ajax is used to convert the whole file into string and extract the required information
     var rawFile = new XMLHttpRequest();
     rawFile.open("GET", file, false);
-    rawFile.onreadystatechange = function ()
-    {
-        if(rawFile.readyState === 4)
-        {
-            if(rawFile.status === 200 || rawFile.status == 0)
-            {
+    rawFile.onreadystatechange = function () {
+        if(rawFile.readyState === 4) {
+            if(rawFile.status === 200 || rawFile.status == 0) {
                 var allText = rawFile.responseText;
                 //alert(allText);
-                get_table_contents(allText);
-
+                get_table_contents(allText); 
             }
         }
     }
     rawFile.send(null);
 }
 
-  function get_table_contents (filetext) {
+  function get_table_contents (filetext) { // this function reads the data and selects the required information
     var table_contents = "";
     var condition = false;
 
@@ -139,7 +135,7 @@ function readTextFile(file)
 
 }) // button click event closes
 
-
+// the resize jquery method syncs the window's size with the iframes
 $(window).resize(function() {
   if($("iframe").length){
     $("iframe").each(function() {
--- Assets/JS/scripts.js
+++ Assets/JS/scripts.js
@@ -1,19 +1,7 @@
-
 $(document).ready(function(){
-	$('h2').hide();	
-	if($("iframes").context != undefined){
-		var iframebody = ($("iframes").context.body);
-		$(iframebody).find("button").hide();
-	};
 
-	$('.myTable td').hide();
-
-$(".tagsToggle").click(function(){
-	 $(".tagContainer").fadeToggle();
-			 
-})
 //to trigger the chart button
-var dataPHP = [];
+var dataPHP = []; // stores the chart data, options and id values of a specific chart 
 var condition = true;
 $("div.buttons > button").click(function(e){
 dataPHP[2] = '"' + this.id + '"';
@@ -26,39 +14,33 @@ var core_id = (this.id).slice(5);
 
 for (var i = 0 ; i < result.length; i++) {
 
-	get_file_details(result[i],core_id);
-	get_chart_details(i);
-	//appendData(i);
+	get_file_details(result[i],core_id); // fetches the chart's data and option and stores into the dataPhp varible
+	get_chart_details(i); // appends the chart_maker.js file into the iframes
 };
 var buttonId = this.id;
-//setTimeout(function(){
 
-$("iframe").each(function(){
+$("iframe").each(function(){ // this function triggers the button the iframe -> to make the chart displayed
 	var sum_button = $(this).contents().find('button#'+buttonId);
 	if(sum_button.length){
 		sum_button.trigger("click");
 	}
 });
 
-//},1000)
 
 
 });
 
 
-function get_chart_details (i) {
+function get_chart_details (i) { // fetches the content inside the chart_maker file
 
 var file_location = "chart_maker.js";
 
 var rawFile = new XMLHttpRequest();
 rawFile.open("GET", file_location, false);
 
-rawFile.onreadystatechange = function ()
-{
-	if(rawFile.readyState === 4)
-	{
-		if(rawFile.status === 200 || rawFile.status == 0)
-		{
+rawFile.onreadystatechange = function () {
+	if(rawFile.readyState === 4) {
+		if(rawFile.status === 200 || rawFile.status == 0) {
 			var allText = rawFile.responseText;
 			appendData(i,allText);
 		}
@@ -70,7 +52,7 @@ rawFile.send(null);
 
 };
 
-function appendData (i,chart_data_function) {
+function appendData (i,chart_data_function) { // appends all the required data that are necessary to make the chart into the iframes
 var iframe = document.getElementsByTagName("iframe")[i];
 var iframe_dom = iframe.contentWindow.document;
 
@@ -102,12 +84,9 @@ var file_location = "Templates/" + filename;
 
 var rawFile = new XMLHttpRequest();
 rawFile.open("GET", file_location, false);
-rawFile.onreadystatechange = function ()
-{
-	if(rawFile.readyState === 4)
-	{
-		if(rawFile.status === 200 || rawFile.status == 0)
-		{
+rawFile.onreadystatechange = function () {
+	if(rawFile.readyState === 4) {
+		if(rawFile.status === 200 || rawFile.status == 0) {
 			var allText = rawFile.responseText;
 			get_chart_data(allText,core_id);
 			get_chart_option(allText,core_id);
@@ -118,7 +97,7 @@ rawFile.onreadystatechange = function ()
 rawFile.send(null);
 };
 
-function get_chart_data (filetext,core_id) {
+function get_chart_data (filetext,core_id) { // filters the whole file and gets the chart data
 var data_id = "data_" + core_id;
 var pos_of_data = (filetext.indexOf(data_id)+(data_id.length));
 var data_contents = ""; var data_condition = false;
@@ -142,7 +121,7 @@ for(var i = pos_of_data ; i < filetext.length; i++) {
 };
 
 
-function get_chart_option (filetext,core_id) {
+function get_chart_option (filetext,core_id) { // filters the whole file and gets the option data
 var option_id = "options_" + core_id;
 var pos_of_option = (filetext.indexOf(option_id)+(option_id.length));
 var option_contents = ""; var option_condition = false;
@@ -162,98 +141,6 @@ var option_contents = ""; var option_condition = false;
 	}
 
 };
-
-
-
-
-
-// Live search 
-$("#live-search").keyup(function(){
-		
-	var filter = $(this).val(), count = 0;
-	$(".tagsFilter div").each(function(){
-		
-		if ($(this).text().search(new RegExp(filter, "i")) < 0) {
-			$(this).fadeOut();
-		}else {
-			$(this).show();
-			// count++;
-		}
-		
-	});
-	// var numberItems = count;
-   
-		
-		
-})//---Live search end here---	
-
-// Tag search History
-var searchTags = "";
-$("#add-the-tag").click(function(){
-
- searchTags += $("#live-search").val() + " ,";
- // if(searchTags===''){
-	 // alert("Nothing to be add");
- // } 
- 
- $( "#Search-History").text(searchTags);
- $("#live-search").val('');
- //searchTags += searchTags;
-})
-
-
-$("#reset-live-search").click(function(){
- $("#live-search").val("");
- $(".tagsFilter div").show();
-}); // Tag Search history end here 
-
-
-
-// for filter the files on click the filter button
-
-$("#applyFilter").click(function(){
-	   $("div.tagContainer").hide();
-	//masterFilterArray = $.unique(masterFilterArray);
-	masterFilterArray = jQuery.unique( masterFilterArray );
-  $("#displaySelectedComponenet").append(masterFilterArray.join("\n"));
-})
-
-// get the value of all selected tags in one Master filter Array
-
-var masterFilterArray = [];
-$('select#cloudProvider').on('change',function(){
-   masterFilterArray.push($(this).find(":selected").val());
-   console.log(masterFilterArray);
-});
-
-$('select#dataBase').on('change',function(){
-	masterFilterArray.push($(this).find(":selected").val());
-	console.log(masterFilterArray);
-});
-$('select#dataBase_config').on('change',function(){
-	masterFilterArray.push($(this).find(":selected").val());
-	console.log(masterFilterArray);
-});
-
-
-$('input[type="date"]').change(function(){
-	masterFilterArray.push(this.value);
-	console.log(masterFilterArray);
-});
-
-
-$('input[type="radio"]').change(function(){
-	masterFilterArray.push(this.value);
-	console.log(masterFilterArray);
-});
-
-$('#maxconn').blur(function(){
-  masterFilterArray.push($('#maxconn').val());
-  console.log(masterFilterArray);
-});
-
-
-
 
 
 });
