427c221ada856f39d46903739a46c48e9a2041b0
Karan Singh Dhir <ksinghdhir1@gmail.com>
2017-12-02 21:15:23 +0530

    directory log feature added that keeps record of the last 3 directories entered

--- server.php
+++ server.php
@@ -1,7 +1,7 @@
 <?php
      $dir_location = $_GET['directory_location'];
      
-     if($dir_location != "") {
+     if($dir_location != "") { // the value is again checked if it's empty of or in case the file is used again for another ajax call
         if (file_exists($dir_location)) {
             $dates = scandir($dir_location,1);
             
@@ -17,5 +17,5 @@
         } else {
             echo "not_available";
         };
-    }
+    } 
 ?>
