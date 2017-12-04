bc5d815971679df44c5e78139f7bfe43dca3a682
Karan Singh Dhir <ksinghdhir1@gmail.com>
2017-12-04 16:44:06 +0530

    script made succinct

--- server.php
+++ server.php
@@ -1,19 +1,8 @@
 <?php
-     $dir_location = $_GET['directory_location'];
-     
-     if($dir_location != "") { // the value is again checked if it's empty of or in case the file is used again for another ajax call
+if(isset($_GET['directory_location'])) { // the value is again checked if it's empty of or in case the file is used again for another ajax call
+        $dir_location = $_GET['directory_location'];
         if (file_exists($dir_location)) {
-            $dates = scandir($dir_location,1);
-            
-            if (($dotkey = array_search('.', $dates)) && ($doubledotkey = array_search('..', $dates))) {
-                unset($dates[$dotkey]);
-                unset($dates[$doubledotkey]);
-            }
-
-            foreach($dates as $name) { 
-                    
-            };
-
+            echo "available";
         } else {
             echo "not_available";
         };
